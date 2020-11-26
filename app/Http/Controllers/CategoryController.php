2766610c1cefda75ab946a15f98190a8e96a9d70
Adam Waheed <adamwaheed@gmail.com>
2020-11-26 22:31:32 +0500

    UI added

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,8 +13,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::all()
-        return view('categories.index',compact('categories'))
+
+
     }
 
     /**
