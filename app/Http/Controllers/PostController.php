e23fa1ba43a1d465d7d35479bad3bb236b4f0023
shaugee <kudaramittey@gmail.com>
2020-12-02 22:07:46 +0500

    upload function implemented in postcontroller update function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -91,6 +91,7 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
+        
         $post->title = $request->title;
         $post->body = $request->body;
         $post->slug = $request->slug;
@@ -105,8 +106,16 @@ class PostController extends Controller
             $post->status = "draft";
         }
 
-        $post->save();
+        
+        if ($post->save()){
+            if($request->has("image")){
+                $file =$request->file("image");
+                $fileName = time().'_'.$file->getClientOriginalName();
+                $request->file("image")->storeAs("uploads", $fileName, "public");
+            }
+        }
         return redirect("/posts");
+
     }
 
     /**
